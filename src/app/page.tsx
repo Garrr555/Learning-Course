955713f621bc68cbb7a9e3156b04a4ebffb12845
mohammad tegar ibrahim <mohammadtegaribrahim@gmail.com>
2025-08-15 20:33:40 +0700

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Button>World Test </Button>
+    <div className="w-full flex items-center justify-center h-screen">
+      <Button size={"lg"}>
+        <Link href={"/workspace"}>WORK SPACE</Link>
+      </Button>
     </div>
   );
 }
